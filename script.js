e46567594503375afcdd8ed4624ea5678cb70cb1
Lucas Rafael <79646846+lukulas@users.noreply.github.com>
2022-03-10 14:04:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else{
+        return false
+    }
+}
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else{
+        return false
+    }
+}
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adcionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else{
+        window.alert('[ERRO] O numero digitado não é aceitavel')
+    }
+    num.value = ''
+    num.focus()
+}
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicone valores antes de finalizar')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} numero cadastrados.</p>`
+        res.innerHTML += `<p> O maior valores informado foi o ${maior}. </p>`
+        res.innerHTML += `<p> O menor valores informado foi o ${menor}. </p>`
+        res.innerHTML += `<p> A soma dos valores informado é  ${soma}. </p>`
+        res.innerHTML += `<p> A media dos valores informado é ${media}. </p>`
+    }
+}
